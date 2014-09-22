dad6f4ca3621e129c594c45b6a7919a918cba563
Jeremy Fox <foxjerem@gmail.com>
2014-09-22 11:43:12 +0100

    Finalised event class

--- GiftAidCalculator.TestConsole/Collaborators.cs
+++ GiftAidCalculator.TestConsole/Collaborators.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace GiftAidCalculatorCollaborators
 {
   public class User
@@ -15,6 +17,50 @@ namespace GiftAidCalculatorCollaborators
     public Admin()
     {
       privilege = "admin";
+    }
+  }
+
+  public class Event
+  {
+    private string type;
+    private static Hashtable promotedTypes = PromotedTypesTable();
+
+    private static Hashtable PromotedTypesTable()
+    {
+      Hashtable tbl = new Hashtable();
+
+      tbl.Add("running", 5);
+      tbl.Add("swimming", 3);
+
+      return tbl;
+    }
+    
+    public Event(string eventType)
+    {
+      setType(eventType);
+    }
+
+    public string Type 
+    {
+      get { return type; }
+    }
+
+    public bool IsPromoted()
+    {
+      return IsPromotedType(type);
+    }
+
+    private void setType(string eventType)
+    {
+      if(IsPromotedType(eventType))
+        type = eventType;
+      else
+        type = "default";
+    }
+
+    private bool IsPromotedType(string eventType)
+    {
+      return promotedTypes.ContainsKey(eventType);
     }
   }
 }
---
+++ GiftAidCalculator.Tests/CollaboratorTests.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using GiftAidCalculatorCollaborators;
+
+namespace GiftAidCalculator.TestConsole
+{
+  [TestFixture]
+  public class GiftAidCalculatorUsersTest
+  {
+    Admin admin = new Admin();
+    User user = new User();
+
+    [Test]
+    public void BeingAnAdmin()
+    {
+      Assert.IsTrue(admin.IsAdmin());
+    }
+
+    [Test]
+    public void NotAnAdmin()
+    {
+      Assert.IsFalse(user.IsAdmin());
+    }
+  }
+
+  [TestFixture]
+  public class GiftAidCalculatorEventTest
+  {
+    Event myEvent;
+    Event runEvent;
+
+    [SetUp]
+    public void Init()
+    {
+      myEvent = new Event("sailing");
+      runEvent = new Event("running");
+    }
+
+    [Test]
+    public void EventType()
+    { 
+      Assert.AreEqual("running", runEvent.Type);
+    }
+
+    [Test]
+    public void EventTypeNotPromoted()
+    {
+      Assert.AreEqual("default", myEvent.Type);
+    }
+
+    [Test]
+    public void EventPromoted()
+    { 
+      Assert.IsTrue(runEvent.IsPromoted());
+    }
+
+    [Test]
+    public void EventNotPromoted()
+    {
+      Assert.IsFalse(myEvent.IsPromoted());
+    }
+  }
+}
